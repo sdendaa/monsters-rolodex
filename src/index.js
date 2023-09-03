cf1483e5a9b2c68f2a854c45a3fa778daa18dc12
sdendaa <sdendaa@gmail.com>
2023-09-03 12:10:15 -0400

    added functional components into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./AppFun";
 import AppFun from "./AppFun";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <App /> */}
     <AppFun />
   </React.StrictMode>
 );
